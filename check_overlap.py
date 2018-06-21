94a00bdea619167e4e41ad32a904a6d70656133a
CherylZR <34064993+CherylZR@users.noreply.github.com>
2018-06-22 00:06:51 +0800

    Update check_overlap.py

--- check_overlap.py
+++ check_overlap.py
@@ -1,7 +1,12 @@
+import csv
+
 def inverse_name(name):
     full_name = name.split()
     full_name[0], full_name[-1] = full_name[-1], full_name[0]
     name_inv = '_'.join([subword for subword in full_name])
+
+def is_number(uchar):
+    return uchar >= u'0' and uchar<=u'9'
 
 def find_idx(name):
     idx = 0
@@ -12,46 +17,85 @@ def find_idx(name):
             return idx
 
 if __name__=='__main__':
-    inname = '.\\files\out_entity\entity_list_all.txt'
-    outname = '.\\files\out_entity\entity_overlap.txt'
-    infile = open(inname, 'r', encoding='utf8')
+    inname = '.\\files\\translist_all.txt'
+    outname = '.\\files\\overlap_trans1.txt'
+    # infile = open(inname, 'r', encoding='utf8')
     outfile = open(outname, 'a', encoding='utf8')
     name_dict = {}
     overlap_dict = {}
     i=0
-    for line in infile.readlines():
-        # i+=1
-        # if i>5000:
-        #     break
-        strip_line = line.strip().replace(' ','')
-        idx = find_idx(strip_line)
-        name = strip_line[idx:].lower()
-        id = strip_line[:idx]
-        id_dict = name_dict.get(name)
-        if id_dict:
-            if overlap_dict.get(name):
-                overlap_dict[name] = overlap_dict[name] + ' %s %s' %(name, id)
+
+    with open(inname, 'r', encoding='utf8') as infile:
+        spamreader = csv.reader(infile, delimiter=' ')
+        for line in spamreader:
+            # i += 1
+            # if i>5:
+            #     break
+            id = line[0]
+            name = line[1].lower()
+            id_dict = name_dict.get(name)
+            if id_dict:
+                if overlap_dict.get(name):
+                    overlap_dict[name] = overlap_dict[name] + ' %s %s' % (name, id)
+                else:
+                    overlap_dict[name] = ' '.join([id_dict, name, id])
+                print('%s have ids [%s, %s]' % (name, id_dict, id))
             else:
-                overlap_dict[name] = ' '.join([id_dict, name, id])
-            print('%s have ids [%s, %s]' % (name, id_dict, id))
-        else:
-            name_dict[name] = id
-            print('[id:%s name:%s] is added' % (id, name))
+                name_dict[name] = id
+                print('[id:%s name:%s] is added' % (id, name))
 
-        name_inv = inverse_name(name)
-        id_inv = name_dict.get(name_inv)
-        if id_inv:
-            if overlap_dict.get(name_inv):
-                overlap_dict[name_inv] = overlap_dict[name_inv] + ' %s %s' % (name, id)
-            else:
-                overlap_dict[name_inv] = ' '.join([id_inv, name, id])
-            print('%s have ids [%s, %s]' % (name_inv, id_inv, id))
+            name_inv = inverse_name(name)
+            id_inv = name_dict.get(name_inv)
+            if id_inv:
+                if overlap_dict.get(name_inv):
+                    overlap_dict[name_inv] = overlap_dict[name_inv] + ' %s %s' % (name, id)
+                else:
+                    overlap_dict[name_inv] = ' '.join([id_inv, name, id])
+                print('%s have ids [%s, %s]' % (name_inv, id_inv, id))
 
-        #TODO: search wiki for single-word name
+                # TODO: search wiki for single-word name
 
-    for key, val in overlap_dict.items():
-        outfile.write(key + ' ' + val + '\n')
-    outfile.close()
+        for key, val in overlap_dict.items():
+            outfile.write(key + ' ' + val + '\n')
+        outfile.close()
+        #
+        # name_inv = inverse_name(name)
+        # id_inv = name_dict.get(name_inv, -1)
+        # if id_same != -1:
+
+    # for line in infile.readlines():
+    #     # i+=1
+    #     # if i>5000:
+    #     #     break
+    #     strip_line = line.strip().replace(' ','')
+    #     idx = find_idx(strip_line)
+    #     name = strip_line[idx:].lower()
+    #     id = strip_line[:idx]
+    #     id_dict = name_dict.get(name)
+    #     if id_dict:
+    #         if overlap_dict.get(name):
+    #             overlap_dict[name] = overlap_dict[name] + ' %s %s' %(name, id)
+    #         else:
+    #             overlap_dict[name] = ' '.join([id_dict, name, id])
+    #         print('%s have ids [%s, %s]' % (name, id_dict, id))
+    #     else:
+    #         name_dict[name] = id
+    #         print('[id:%s name:%s] is added' % (id, name))
+    #
+    #     name_inv = inverse_name(name)
+    #     id_inv = name_dict.get(name_inv)
+    #     if id_inv:
+    #         if overlap_dict.get(name_inv):
+    #             overlap_dict[name_inv] = overlap_dict[name_inv] + ' %s %s' % (name, id)
+    #         else:
+    #             overlap_dict[name_inv] = ' '.join([id_inv, name, id])
+    #         print('%s have ids [%s, %s]' % (name_inv, id_inv, id))
+    #
+    #     #TODO: search wiki for single-word name
+    #
+    # for key, val in overlap_dict.items():
+    #     outfile.write(key + ' ' + val + '\n')
+    # outfile.close()
         #
         # name_inv = inverse_name(name)
         # id_inv = name_dict.get(name_inv, -1)
